5630963d5b507bc52426340759e18318072ec9c6
Gauss-Gang <85713806+Gauss-Gang@users.noreply.github.com>
2021-09-18 00:03:46 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,8 +17,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 module.exports = {
   solidity: "0.8.7",
